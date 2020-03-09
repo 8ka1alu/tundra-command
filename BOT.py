9f9ca054164383f59c85f9b69cc07b3a937fc08b
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-03-09 20:54:06 +0900

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -35,7 +35,6 @@ loaded = datetime.datetime.now(
 loaded = loaded.strftime('%Y年%m月%d日 %H:%M')
 
 conn = sqlite3.connect('discordbot.db')
-c = conn.cursor()
 
 # ここからコマンド
 
@@ -178,6 +177,7 @@ async def mp(ctx):
 
 @mp.command()
 async def create(ctx,member: discord.Member):
+    c = conn.cursor() 
     c.execute(f"CREATE TABLE {member} ({member} TEXT, price INTEGER)")
     await ctx.send("登録しました。")
 
